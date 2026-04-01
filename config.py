a366e808fefb100b65aa3e24899ca51caafad596
dheerajgiri1337-collab <dheerajgiri1337@gmail.com>
2026-04-01 16:39:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
 # -----------------------------------------------
 UNSPLASH_QUERY = 'animal baby'
 # -----------------------------------------------
-ADMIN_BOT_USERNAME = "Dheeraj_1337" #without @
+ADMIN_BOT_USERNAME = "@Dheeraj_1337" #without @
 
 THUMB_URL = os.environ.get("THUMB_URL", "https://i.ibb.co/B5G4jZ8q/IMG-20260401-150751.jpg")
 
